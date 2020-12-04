c541b64c5ca400c94eac3347e9c1135917be59ed
ttubuckee <tommy1003@naver.com>
2020-12-04 12:46:37 +0900

    Update PageController.js, QuestionBody.js ProgressBar.js

--- src/components/PageController.js
+++ src/components/PageController.js
@@ -5,6 +5,7 @@ import Loading from './Loading';
 import Result from './Result';
 import Start from './Start';
 import QuestionBody from './QuestionBody';
+
 const PageController = (props) => {
     const startImgSrc = 'https://post-phinf.pstatic.net/MjAxODA4MDlfMjk2/MDAxNTMzNzkyMjAzMzA2.eN6ftc0F8v7_-RafQW35jposp2qa1JBnaW-QScAu3GIg.JQDWvH38-z54nTEWjL2krDWtHq3giqo4stIz5Pw-V3cg.PNG/1.png?type=w1200'
     React.useEffect(()=>{
@@ -16,10 +17,10 @@ const PageController = (props) => {
             return <Start imgSrc={startImgSrc} onPress={props.onPress}/>
             break;
         case 1: // 1번 질문
-            return <QuestionBody numOfQuestions={4} style={{marginTop:'107px', width:'88%'}} onPress={props.onPress} text={'버튼'}/>
+            return <QuestionBody numOfQuestions={4} style={{marginTop:'107px', width:'88%'}} onPress={props.onPress} text={'버튼'} pageIdx={props.pageIdx}/>
             break;
         default: // case 1~13
-            return <QuestionBody numOfQuestions={2} onPress={props.onPress} style={{marginTop:'107px', width:'88%'}} text={'버튼'}/>
+            return <QuestionBody numOfQuestions={2} onPress={props.onPress} style={{marginTop:'107px', width:'88%'}} text={'버튼'} pageIdx={props.pageIdx}/>
             break;
         case 14: // 로딩 화면
             return <Loading onPress={props.onPress} text={'버튼'}/>
--- src/components/QuestionBody.js
+++ src/components/QuestionBody.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import classNames from 'classnames';
+import ProgressBar from './ProgressBar';
+
 const QuestionBody = (props) => {
     const { text, numOfQuestions } = props; // props(read-only)
     React.useEffect(()=>{
         //setTest(true);
     }, [])
+    const bottom_wrapper_style = {
+        width:"100%",
+        margin: "0 auto"
+    }
+    const wrapperStyle = {
+        "width": "100%",
+        "height": "8px",
+        "margin": "10px auto",
+        "padding": "0px",
+        "backgroundColor": "#EAEAEA"
+    }
     return(
-    <div className={classNames('button-wrapper')} onClick={props.onPress} style={props.style}>
-        <div>{text}</div>
-    </div>)
+    <div className={"bottom-wrapper"} style={bottom_wrapper_style}>
+        <div className={classNames('button-wrapper')} onClick={props.onPress} style={props.style}>
+            <div>{text}</div>
+        </div>   
+        <span style={{fontSize:"18px",fontWeight:"bold", textAlign:"right", display:"block", marginTop:"56px", marginRight:"10px"}}>{props.pageIdx}/{14}</span>
+        <ProgressBar wrapperStyle={wrapperStyle} progress={props.pageIdx} questions_len={14}/>
+    </div>
+    )
 }
 
 export default QuestionBody;

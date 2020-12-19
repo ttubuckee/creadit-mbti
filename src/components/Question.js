39e185cc4217ad25d0a3132a28a8985dd3f426f2
wooyoung <souljit2@gmail.com>
2020-12-19 19:43:21 +0900

    Modify question css

--- src/components/Question.js
+++ src/components/Question.js
@@ -4,12 +4,15 @@ import { getMBTIQuestions } from '../modules/GeneralFunction';
 const Question = (props) => {
     const { questionIdx } = props; // props(read-only)
     const questions = getMBTIQuestions();
+    
     React.useEffect(()=>{
         //setTest(true);
     }, [])
     return(
     <div className={classNames('question-wrapper')} style={props.style}>
-        <div className={classNames('question-text')}>{questions[questionIdx]}</div>
+        <div className={classNames('question-text')}>{questions[questionIdx].split('\n').map( line => {
+            return <div>{line}</div>
+        })}</div>
     </div>) 
 }
 
